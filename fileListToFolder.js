5f689a4997abf49ecce1d44e3c9d04247c193ae3
Steven Bell <thebells1111@gmail.com>
2020-10-05 22:16:26 -0500

    delete unused files, work on fileListToFolder

--- fileListToFolder.js
+++ fileListToFolder.js
@@ -1,6 +1,6 @@
-let fileList = require("./file.json");
+let fileList = require("./fileList.json");
 
-function fileListToFolder(fileList) {
+async function fileListToFolder(fileList) {
   let folder = {};
 
   folder.projectNames = new Set();
@@ -26,36 +26,18 @@ function fileListToFolder(fileList) {
         folder[project][chapter][app] = folder[project][chapter][app] || {
           folders: [],
         };
-        let idcount = 0;
-        var idmap = {};
 
-        add(directories);
+        let splitName = directories.join("/").split(".");
+        let type = splitName.pop();
+        let name = splitName.join(".");
 
-        function add(dirs) {
-          if (!dirs.length) return "#";
-          var name = dirs.join("/");
-          if (name in idmap) return idmap[name];
-          var dir = dirs.pop();
-          var parent = add(dirs);
-          var id = "ajson" + ++idcount;
-          folder[project][chapter][app].folders.push({
-            id: id,
-            parent: parent,
-            text: dir,
-          });
-          return (idmap[name] = id);
-        }
+        let component = {};
+        component.name = name;
+        component.type = type;
+        component.source = "source"; //change to async file reader
 
-        // let splitName = directories.join("/").split(".");
-        // let type = splitName.pop();
-        // let name = splitName.join(".");
-
-        // let component = {};
-        // component.name = name;
-        // component.type = type;
-        // component.source = "source"; //change to async file reader
-
-        // folder[project][chapter][app].components.push(component);
+        folder[project][chapter][app].folders.push(component);
+        folder = await handleFileUpload(directories.join("/"));
       } else {
         folder[project][chapter].text = app; //change this to actual text.md
       }
@@ -67,6 +49,94 @@ function fileListToFolder(fileList) {
     folder[project].chapterNames = [...folder[project].chapterNames].sort();
   });
   return folder;
+}
+
+async function handleFileUpload(arr) {
+  let newFiles = [];
+  for (const file of arr) {
+    let f = {};
+    f.webkitRelativePath = file.webkitRelativePath;
+    f.name = file.name;
+    //f.source = await readFileAsync(file);
+    newFiles.push(f);
+  }
+  files = filesToTreeNodes(newFiles);
+  console.log(files);
+}
+
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+
+    reader.onerror = reject;
+
+    reader.readAsText(file);
+  });
+}
+
+function filesToTreeNodes(arr) {
+  var tree = {};
+  async function addnode(obj) {
+    var splitpath = obj.webkitRelativePath.split("/");
+    var ptr = tree;
+    for (let i = 0; i < splitpath.length; i++) {
+      let node = {
+        name: splitpath[i],
+        type: "directory",
+      };
+      if (i == splitpath.length - 1) {
+        let splitName = splitpath[i].split(".");
+        node.type = splitName.pop();
+        node.name = splitName.join(".");
+        node.source = obj.source;
+      }
+      ptr[splitpath[i]] = ptr[splitpath[i]] || node;
+      ptr[splitpath[i]].children = ptr[splitpath[i]].children || {};
+      ptr = ptr[splitpath[i]].children;
+    }
+  }
+  function objectToArr(node) {
+    Object.keys(node || {})
+      .map((k) => {
+        if (node[k].children) {
+          objectToArr(node[k]);
+        }
+      })
+      .sort((a, b) => {
+        let aa = a.name.toLowerCase(),
+          bb = b.name.toLowerCase();
+
+        if (aa < bb) {
+          return -1;
+        }
+        if (aa > bb) {
+          return 1;
+        }
+        return 0;
+      });
+    if (node.children) {
+      node.children = Object.values(node.children).sort((a, b) => {
+        let aa = a.name.toLowerCase(),
+          bb = b.name.toLowerCase();
+
+        if (aa < bb) {
+          return -1;
+        }
+        if (aa > bb) {
+          return 1;
+        }
+        return 0;
+      });
+      node.children.forEach(objectToArr);
+    }
+  }
+  arr.map(addnode);
+  objectToArr(tree);
+  return Object.values(tree);
 }
 let list = fileListToFolder(fileList);
 console.log(JSON.stringify(list, null, 2));
